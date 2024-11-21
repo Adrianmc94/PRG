12863686bd378329d0f4434f1d6338324cb97b73
Adrian Miguez <amiguezcampos@danielcastelao.org>
2024-11-21 13:12:23 +0100

    ejrs

--- Boletin6/src/ej1.java
+++ Boletin6/src/ej1.java
@@ -1,31 +1,25 @@
 public class ej1 {
-    
+
+
     public static void mostrarNumerosEntre(int num1, int num2) {
+        if (num1 > num2) {
 
-        if (num1 < num2) {
-            for (int i = num1; i <= num2; i++) {
-                System.out.println(i);
-            }
-        } else if (num1 > num2) {
-
-            for (int i = num1; i >= num2; i--) {
-                System.out.println(i);
-            }
-        } else {
-
-            System.out.println("Os números son iguais: " + num1);
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
-    }
 
+        for (int i = num1 + 1; i < num2; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
 
-        mostrarNumerosEntre(num1, num2);
+        mostrarNumerosEntre(5, 10);
+        mostrarNumerosEntre(10, 5);
     }
 }
-
-
 
 
---
+++ Boletin6/src/ej2.java
@@ -0,0 +1,11 @@
+public class ej2 {
+
+    public static void eco(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ecoo...");
+        }
+    }
+    public static void main(String[] args) {
+        eco(5);
+    }
+}
