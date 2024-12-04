fa047ed6929d4e139505bcceddb3b95ba522713a
Adrian Miguez <amiguezcampos@danielcastelao.org>
2024-12-04 14:26:06 +0100

    ejrs

---
+++ prueba_debug/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CuentaAtras"},{"p":"<Unnamed>","l":"FUNCIONES"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
