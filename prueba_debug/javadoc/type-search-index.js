d0185c9efbb791b9f21dc4672e918e44b46f99db
AdrianMiguez <amiguezcampos@danielcastelao.org>
2024-12-19 08:53:17 +0100

    Delete prueba_debug directory

--- prueba_debug/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"CuentaAtras"},{"p":"<Unnamed>","l":"FUNCIONES"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
